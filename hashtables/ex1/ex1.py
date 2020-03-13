6401802b3600d998a6029dabd3f29b74f9b4d4e5
Rory Flint <roryflintphoto@gmail.com>
2020-03-13 09:17:18 +0000

    Pseudo added for hastables ex 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,9 +9,16 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    # Check length isn't 1, if so return None ( as per test spec)
+    # Loop through the weights list (using length), insert each one into the hash table.
+    # Key is the weight, the value is the index in the list
+
+    # Loop through list again, passing in the limit - the current weight into the retrieve function
+    # Assign each return value to a temporary variable.
+    # Check if the the variable exists (aka, true). If it's false, continue
+    # If it's true, we have a match to the limit.
+    # Create a new tuple using the returned value (the index of the matching item), and the current
+    # number we are on in the loop (the index of the matching item)
 
     return None
 
